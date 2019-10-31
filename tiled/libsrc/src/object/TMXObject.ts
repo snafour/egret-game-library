420907c5b79e24822cb5c193fe1ac84020e8f86e
snafour <snafour@163.com>
2019-10-31 23:26:28 +0800

    _properties

--- tiled/libsrc/src/object/TMXObject.ts
+++ tiled/libsrc/src/object/TMXObject.ts
@@ -29,7 +29,7 @@ module tiled{
 		private _flippedAD: boolean;
 		private _flipped: boolean;
 
-		private _properties: Array<tiled.TMXProperty>;
+		public _properties:any;
 		
 		/**
 		 * 创建一个Tile对象实例
